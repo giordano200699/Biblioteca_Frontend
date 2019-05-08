7e18cdfdb871667b1bf1704934177950235f1c06
giordano200699 <giordano200699@hotmail.com>
2019-05-07 20:03:16 -0500

    Arreglando Merge

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -13,14 +13,10 @@ export class UsersService {
                       'Yc0jNm44loPty2ZK1nI0qisrt2Xwq0nFhdp8H3kdpyL5wVZLH7EpSE6IO0cHAOGOfSpJjF3' +
                       '6eiCuXJ3gkOfX8C4n';
 
-<<<<<<< HEAD
   clave: string = 'clave=QDm6pbKeVwWikPvpMSUYwp0tNnxcaLoY' +
                   'LnyvLQ4ISV39uQOgsjTEjS0UNlZHwbxl2Ujf30S31CSKndwpkFeubt5gJHTgFlq7LeIaS' +
                   'Yc0jNm44loPty2ZK1nI0qisrt2Xwq0nFhdp8H3kdpyL5wVZLH7EpSE6IO0cHAOGOfSpJjF3' +
                   '6eiCuXJ3gkOfX8C4n';
-=======
-  usersURL:string = "https://bibliotecabackend.herokuapp.com/usuarios?Content-Type=application/json&clave=QDm6pbKeVwWikPvpMSUYwp0tNnxcaLoYLnyvLQ4ISV39uQOgsjTEjS0UNlZHwbxl2Ujf30S31CSKndwpkFeubt5gJHTgFlq7LeIaSYc0jNm44loPty2ZK1nI0qisrt2Xwq0nFhdp8H3kdpyL5wVZLH7EpSE6IO0cHAOGOfSpJjF36eiCuXJ3gkOfX8C4n"
->>>>>>> 13b839d7d6eb890ae3db20c5f822c79129fa4015
 
   // tslint:disable-next-line:no-inferrable-types
   userURL: string = 'https://bibliotecabackend.herokuapp.com/usuarios';
