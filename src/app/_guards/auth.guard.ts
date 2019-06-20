3decf47d508c68b6bf06b85d937046d5c80cb152
giordano200699 <giordano200699@gmail.com>
2019-06-20 10:52:42 -0500

    Seguridad con las rutas no permitidas

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -14,7 +14,46 @@ export class AuthGuard implements CanActivate {
         const currentCuenta = this.authenticationService.currentCuentaValue;
         if (currentCuenta) {
             // logged in so return true
-            return true;
+            const autentificado = JSON.parse(this.authenticationService.obtenerAutentificado());
+            const ruta = route.routeConfig.path;
+            if(autentificado.tipoUsuarioId < 11){
+                //Es usuario
+                if(ruta == "administrador/estadisticas" ||
+                ruta == "administrador/libros/temas/:id"||
+                ruta == "administrador/prestamos"||
+                ruta == "users"||
+                ruta == "profile/:dni"||
+                ruta == "update/:dni"||
+                ruta == "user/:id"||
+                ruta == "libros"||
+                ruta == "libro/:id"||
+                ruta == "profil/:id"||
+                ruta == "edit/:id"||
+                ruta == "editoriales"||
+                ruta == "editorial/:id"||
+                ruta == "edita/:id"||
+                ruta == "autores"||
+                ruta == "autor/:id"||
+                ruta == "editar/:id"){
+
+                    this.router.navigate(['/pedidos'], { queryParams: { returnUrl: state.url } });
+                    return false;
+                }else{
+                    return true;
+                }
+            }else{
+                //Es administrador
+                if(ruta == "pedidos"||
+                ruta == "solicitados"||
+                ruta == "prestamos"||
+                ruta == "historial"||
+                ruta == "pedido/:id"){
+                    this.router.navigate(['/administrador/prestamos'], { queryParams: { returnUrl: state.url } });
+                    return false;
+                }else{
+                    return true;
+                }
+            }
         }
 
         // not logged in so redirect to login page with the return url
