5b99a2c590521965dbbf04360b8e759afbf733ee
Fernando Fuentes <fernando.fuentes@unmsm.edu.pe>
2019-07-03 02:03:27 -0500

    BuscarUsuario-paginacion

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -4,20 +4,26 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
-  templateUrl: './users.component.html'
+  templateUrl: './users.component.html',
+  styles: []
 })
 export class UsersComponent implements OnInit {
 
   users: any[] = [];
   usuarioBoton;
   closeResult: string;
+  filterName  = '';
+  usuarios = [];
+  pag: number = 1;
 
   constructor(private usersService: UsersService,
               private router: Router) {
 
+    var mithis = this;
     this.usersService.getUsers()
         .subscribe( data => {
           this.users = data;
+          mithis.usuarios = [...data];
         });
   }
 
@@ -33,6 +39,23 @@ export class UsersComponent implements OnInit {
       });
   }
 
+  filtro() {
+    this.vaciar();
+    
+    if (this.filterName === '' || this.filterName.length < 3) {
+      return;
+    }
+    for (const usuario of this.users) {
+      if (usuario.codigo.toLowerCase().indexOf(this.filterName.toLowerCase()) > -1) {
+        this.usuarios.push(usuario);
+      }
+    }
+  }
+  vaciar() {
+    while (this.usuarios.length > 0) {
+      this.usuarios.pop();
+    }
+  }
   modalBoton(usuario) {
     this.usuarioBoton = usuario;
   }
